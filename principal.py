6e993ff4ae7acc91666801a9569a8d2fa083b4fe
drachirgc <drachirgc@gmail.com>
2023-07-01 22:21:40 -0400

    Agregadas las variables y el primer if

--- principal.py
+++ principal.py
@@ -1,3 +1,13 @@
 """Esto es una prueba para practicar con Git"""
 
 print("Hola Git")
+
+a = 4
+b = 17
+c = a + b
+print (c)
+
+if c > 15:
+    print("el numero es mayor que 15")
+else:
+    print("esto es una tonteria")
